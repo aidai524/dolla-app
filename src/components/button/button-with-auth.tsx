d2fafe8aaf5d211f589546daeea0fe1be875b66e
Luna <luna.spark9234@proton.me>
2025-06-28 08:27:47 -0500

    add switch chain.

--- src/components/button/button-with-auth.tsx
+++ src/components/button/button-with-auth.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from "@/contexts/auth";
 import Button from ".";
+import useChainId from "@/hooks/use-chain-id";
+import useToast from "@/hooks/use-toast";
 
 export default function ButtonWithAuth({
   children,
@@ -9,11 +11,20 @@ export default function ButtonWithAuth({
   ...rest
 }: any) {
   const { userInfo, login, logining } = useAuth();
+  const { chainId, switchToBerachain } = useChainId();
 
   if (!userInfo) {
     return (
       <Button onClick={login} className={className} loading={logining}>
         Connect Wallet
+      </Button>
+    );
+  }
+
+  if (chainId !== 80094) {
+    return (
+      <Button className={className} onClick={switchToBerachain} {...rest}>
+        Switch Network
       </Button>
     );
   }
