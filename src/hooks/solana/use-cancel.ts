8384f86b95a5aa97a69c806898bf48722c935503
Luna <luna.spark9234@proton.me>
2025-07-02 14:41:20 +0800

    add cancel, claim funds and claim slash.

---
+++ src/hooks/solana/use-cancel.ts
@@ -0,0 +1,139 @@
+import { useState } from "react";
+import { BASE_TOKEN, QUOTE_TOKEN } from "@/config/btc";
+import useToast from "@/hooks/use-toast";
+import reportHash from "@/utils/report-hash";
+import * as anchor from "@coral-xyz/anchor";
+import useProgram from "./use-program";
+import {
+  getState,
+  getNextOrderId,
+  getPool,
+  getAssociatedTokenAddress,
+  getWrapToSolIx
+} from "./helpers";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID
+} from "@solana/spl-token";
+import { useSolanaWallets } from "@privy-io/react-auth";
+import {
+  useSignTransaction,
+  useSendTransaction
+} from "@privy-io/react-auth/solana";
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction
+} from "@solana/web3.js";
+
+export default function useCancel({
+  onCancelSuccess
+}: {
+  onCancelSuccess?: () => void;
+}) {
+  const [canceling, setCanceling] = useState(false);
+  const { wallets } = useSolanaWallets();
+  const toast = useToast();
+  const { program, provider } = useProgram();
+  const { signTransaction } = useSignTransaction();
+  const { sendTransaction } = useSendTransaction();
+  const onCancel = async (orderId: number) => {
+    if (!wallets.length || !orderId) {
+      toast.fail({ title: "Please connect your wallet" });
+      return;
+    }
+    const payer = wallets[0];
+    try {
+      setCanceling(true);
+      const state = getState(program);
+
+      const pool = await getPool(program, provider, state.pda, orderId);
+
+      const userBaseAccount = await getAssociatedTokenAddress(
+        new PublicKey(BASE_TOKEN.address),
+        new PublicKey(payer.address),
+        provider
+      );
+
+      const userQuoteAccount = await getAssociatedTokenAddress(
+        new PublicKey(QUOTE_TOKEN.address),
+        new PublicKey(payer.address),
+        provider
+      );
+
+      const poolBaseAccount = await getAssociatedTokenAddress(
+        new PublicKey(BASE_TOKEN.address),
+        new PublicKey(pool.pda.toString()),
+        provider
+      );
+
+      const poolQuoteAccount = await getAssociatedTokenAddress(
+        new PublicKey(QUOTE_TOKEN.address),
+        new PublicKey(pool.pda.toString()),
+        provider
+      );
+
+      let wrapTx: any[] = [];
+      if (
+        BASE_TOKEN.address.toString() ==
+        "So11111111111111111111111111111111111111112"
+      ) {
+        wrapTx = getWrapToSolIx(payer, userBaseAccount, 100000000);
+      }
+
+      let cancelPoolAccounts = {
+        dollaState: state.pda,
+        poolState: pool.pda,
+        baseMint: new PublicKey(BASE_TOKEN.address),
+        quoteMint: new PublicKey(QUOTE_TOKEN.address),
+        userBaseAccount: userBaseAccount?.address,
+        userQuoteAccount: userQuoteAccount?.address,
+        poolBaseAccount: poolBaseAccount?.address,
+        poolQuoteAccount: poolQuoteAccount?.address,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        user: new PublicKey(payer.address),
+        systemProgram: anchor.web3.SystemProgram.programId
+      };
+      const tx: TransactionInstruction = await program.methods
+        .cancelPool()
+        .accounts(cancelPoolAccounts)
+        .instruction();
+      const batchTx = new Transaction().add(...wrapTx).add(tx);
+      batchTx.feePayer = new PublicKey(import.meta.env.VITE_SOLANA_OPERATOR);
+      // Get the latest blockhash
+      const { blockhash } = await provider.connection.getLatestBlockhash();
+      batchTx.recentBlockhash = blockhash;
+
+      // const signedTx = await signTransaction({
+      //   transaction: tx,
+      //   connection: provider.connection
+      // });
+
+      const receipt = await sendTransaction({
+        transaction: batchTx,
+        connection: provider.connection
+      });
+      console.log("receipt:", receipt);
+      // Report hash for tracking
+      reportHash({
+        chain: "solana",
+        user: payer.address,
+        hash: receipt.signature,
+        block_number: blockhash
+      });
+
+      onCancelSuccess?.();
+    } catch (error) {
+      console.error("Create error:", error);
+      throw error;
+    } finally {
+      setCanceling(false);
+    }
+  };
+
+  return {
+    canceling,
+    onCancel
+  };
+}
---
+++ src/hooks/solana/use-claim-slash.ts
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { BASE_TOKEN, QUOTE_TOKEN } from "@/config/btc";
+import useToast from "@/hooks/use-toast";
+import reportHash from "@/utils/report-hash";
+import * as anchor from "@coral-xyz/anchor";
+import useProgram from "./use-program";
+import {
+  getState,
+  getPool,
+  getAssociatedTokenAddress,
+  getBuyState
+} from "./helpers";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID
+} from "@solana/spl-token";
+import { useSolanaWallets } from "@privy-io/react-auth";
+import {
+  useSignTransaction,
+  useSendTransaction
+} from "@privy-io/react-auth/solana";
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction
+} from "@solana/web3.js";
+
+export default function useClaimSlash({
+  onClaimSuccess
+}: {
+  onClaimSuccess?: () => void;
+}) {
+  const [claiming, setClaiming] = useState(false);
+  const { wallets } = useSolanaWallets();
+  const toast = useToast();
+  const { program, provider } = useProgram();
+  const { signTransaction } = useSignTransaction();
+  const { sendTransaction } = useSendTransaction();
+  const onClaim = async (orderId: number) => {
+    if (!wallets.length || !orderId) {
+      toast.fail({ title: "Please connect your wallet" });
+      return;
+    }
+    const payer = wallets[0];
+    try {
+      setClaiming(true);
+      const state = getState(program);
+
+      const pool = await getPool(program, provider, state.pda, orderId);
+      const buyerState = await getBuyState(
+        program,
+        provider,
+        pool.pda,
+        new PublicKey(payer.address)
+      );
+      const userQuoteAccount = await getAssociatedTokenAddress(
+        new PublicKey(QUOTE_TOKEN.address),
+        new PublicKey(payer.address),
+        provider
+      );
+
+      const poolQuoteAccount = await getAssociatedTokenAddress(
+        new PublicKey(QUOTE_TOKEN.address),
+        new PublicKey(pool.pda.toString()),
+        provider
+      );
+
+      let claimSlashFundsAccounts = {
+        dollaState: state.pda,
+        poolState: pool.pda,
+        buyerState: buyerState.pda,
+        quoteMint: new PublicKey(QUOTE_TOKEN.address),
+        userQuoteAccount: userQuoteAccount?.address,
+        poolQuoteAccount: poolQuoteAccount?.address,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        user: new PublicKey(payer.address),
+        systemProgram: anchor.web3.SystemProgram.programId
+      };
+      const tx: TransactionInstruction = await program.methods
+        .claimSlashFunds()
+        .accounts(claimSlashFundsAccounts)
+        .instruction();
+      const batchTx = new Transaction().add(tx);
+      batchTx.feePayer = new PublicKey(import.meta.env.VITE_SOLANA_OPERATOR);
+      // Get the latest blockhash
+      const { blockhash } = await provider.connection.getLatestBlockhash();
+      batchTx.recentBlockhash = blockhash;
+
+      // const signedTx = await signTransaction({
+      //   transaction: tx,
+      //   connection: provider.connection
+      // });
+
+      const receipt = await sendTransaction({
+        transaction: batchTx,
+        connection: provider.connection
+      });
+      console.log("receipt:", receipt);
+      // Report hash for tracking
+      reportHash({
+        chain: "solana",
+        user: payer.address,
+        hash: receipt.signature,
+        block_number: blockhash
+      });
+
+      onClaimSuccess?.();
+    } catch (error) {
+      console.error("Create error:", error);
+      throw error;
+    } finally {
+      setClaiming(false);
+    }
+  };
+
+  return {
+    claiming,
+    onClaim
+  };
+}
