3409673c363e57d76b2d35755f6a3e64c1517b08
Luna <luna.spark9234@proton.me>
2025-07-20 07:39:01 +0800

    remove useless code.

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,6 @@
 // export const HOST_API = import.meta.env.DEV ? "/api" : "/";
 export const HOST_API = "https://test-api.dolla.market";
 
-("0x215b5B46B85F02A04DEd500150A621022C75e90e");
-
 export const PURCHASE_TOKEN = {
   address: "0x26591f0f2bbab1bb3cd457eE1dfd80EAE1474C6c",
   decimals: 18,
