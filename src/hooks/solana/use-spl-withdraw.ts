b4264afe1e9b4151cae6c95e01cadc0efd1a8400
ben-dagege <ben-dage@proton.me>
2025-07-21 18:26:18 +0800

    add withdraw

---
+++ src/hooks/solana/use-spl-withdraw.ts
@@ -0,0 +1,65 @@
+import { useSendTransaction, useSolanaWallets } from '@privy-io/react-auth/solana';
+import { getAssociatedTokenAddress } from "./helpers";
+import useProgram from "./use-program";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { PublicKey, Transaction } from "@solana/web3.js";
+import { createTransferInstruction } from "@solana/spl-token";
+import { useState } from "react";
+import reportHash from '@/utils/report-hash';
+
+export default function useSplWithdraw({ token, amount, targetAddress }: { token: any, amount: number, targetAddress: string }) {
+    const { sendTransaction } = useSendTransaction();
+    const [loading, setLoading] = useState(false);
+    const { provider } = useProgram();
+    const { wallets } = useSolanaWallets();
+
+    const withdraw = async () => {
+        const { address, instruction, account: tokenAccount } = await getAssociatedTokenAddress(token.address, targetAddress, provider);
+        const payer = wallets[0];
+
+        console.log(payer);
+
+        const owerPublicKey = new PublicKey(payer.address);
+
+        const transaction: Transaction = new Transaction()
+
+        if (instruction) {
+            transaction.add(instruction);
+        }
+
+        transaction.add(
+            createTransferInstruction(
+                owerPublicKey,
+                tokenAccount,
+                owerPublicKey,
+                amount,
+                [owerPublicKey]
+            )
+        );
+
+        const latestBlockhash = await provider.connection.getLatestBlockhash();
+        transaction.recentBlockhash = latestBlockhash.blockhash;
+        transaction.feePayer = owerPublicKey;
+
+        const receipt = await sendTransaction({
+            transaction: transaction,
+            connection: provider.connection
+        });
+
+        reportHash({
+            chain: "solana",
+            user: payer.address,
+            hash: receipt.signature,
+            block_number: latestBlockhash.blockhash
+        });
+
+        console.log(receipt);
+
+        return receipt;
+    }
+
+    return {
+        withdraw,
+        loading,
+    }
+}
