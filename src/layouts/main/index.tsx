c264981df8c01a67a20ff71e504f086db0697387
Luna <luna.spark9234@proton.me>
2025-07-22 08:52:42 +0800

    add points section.

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -2,13 +2,10 @@ import AvatarAction from "./avatar-action";
 import { Outlet } from "react-router-dom";
 import Button from "@/components/button";
 import { useAuth } from "@/contexts/auth";
-import { CoinIcon } from "./icons";
-import { addThousandSeparator } from "@/utils/format/number";
-import useUserInfoStore from "@/stores/use-user-info";
+import Points from "@/sections/points";
 
 export default function MainLayout() {
   const { userInfo, login } = useAuth();
-  const { prize } = useUserInfoStore();
 
   return (
     <div className="h-screen overflow-hidden bg-black relative">
@@ -20,18 +17,7 @@ export default function MainLayout() {
           className="w-[78px] h-[39px] absolute left-[10px] top-[4px] z-[20]"
         />
         <div className="absolute right-[10px] top-[10px] z-[20] flex items-center gap-[36px]">
-          <div className="flex items-center gap-[8px]">
-            <CoinIcon />
-            <span
-              className="text-[#FFEF43] text-[20px] font-bold font-[AlfaSlabOne]"
-              style={{
-                WebkitTextStrokeWidth: "1px",
-                WebkitTextStrokeColor: "#5E3737"
-              }}
-            >
-              x{addThousandSeparator(prize.points.toString())}
-            </span>
-          </div>
+          <Points />
           {/* <div className="flex items-center gap-[8px]">
             <TicketIcon />
             <span
---
+++ src/sections/points/index.tsx
@@ -0,0 +1,21 @@
+import CoinIcon from "@/components/icons/coin";
+import useUserInfoStore from "@/stores/use-user-info";
+import { addThousandSeparator } from "@/utils/format/number";
+
+export default function Points() {
+  const { prize } = useUserInfoStore();
+  return (
+    <div className="flex items-center gap-[8px]">
+      <CoinIcon />
+      <span
+        className="text-[#FFEF43] text-[20px] font-bold font-[AlfaSlabOne]"
+        style={{
+          WebkitTextStrokeWidth: "1px",
+          WebkitTextStrokeColor: "#5E3737"
+        }}
+      >
+        x{addThousandSeparator(prize.points.toString())}
+      </span>
+    </div>
+  );
+}
